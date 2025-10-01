d7382b670b2b876fbeee1d969e3a36e5e1ee5d7e
thanaphon <thanaphon@ucconnect.co.th>
2025-10-01 20:11:51 +0700

    cicd#5

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,11 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get('test')
+  getTest(): string {
+    return this.appService.getHello();
+  }
+
   @Get()
   getHello(): string {
     return this.appService.getHello();
