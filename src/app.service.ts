863afde5c86d9a755956e64f30810b70a476ae09
thanaphon <thanaphon@ucconnect.co.th>
2025-10-02 13:39:30 +0700

    push

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getTest(): string {
-    return 'Hello World! 12321312';
+    return 'Hello World! 5555';
   }
 
   getHello(): string {
